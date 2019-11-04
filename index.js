77303cb18791d728bf2d0521baaf6d196f46c7d6
Michelle Sage <michelle2sage@gmail.com>
2019-11-04 02:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var greeting = "Hello, everybody!";
 'Hello, everybody!'
 var specialGuest = "Neil deGrasse Tyson"
-"Hello, " + specialGuest + "!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
-
+'"Hello, " + specialGuest + "!"'
 var topic = "space";
 
 var conversation = `${topic}`;
